e94e37c40320ad231cb433e07c5a125287e2eb60
Tuvshu <tuvshinjargal.b@gmail.com>
2025-11-20 10:18:24 -0600

    feat: add Laravel Wayfinder guidelines for route generation

--- .ai/guidelines/general.blade.php
+++ .ai/guidelines/general.blade.php
@@ -1,3 +1,11 @@
 # General Guidelines
 
 - Don't include any superfluous PHP Annotations, except ones that start with `@` for typing variables.
+
+# Laravel Wayfinder Guidelines
+
+When you generate the routes, use `--with-form` so that `Wayfinder` can create form request classes for you, like so;
+
+```shell
+php artisan wayfinder:generate --with-form
+```
