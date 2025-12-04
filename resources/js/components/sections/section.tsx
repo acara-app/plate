13eacc4c05efd76f2017f09e01c5a32f598eaa12
Tuvshu <tuvshinjargal.b@gmail.com>
2025-12-04 16:31:53 -0600

    style: fix prettier formatting for section component

--- resources/js/components/sections/section.tsx
+++ resources/js/components/sections/section.tsx
@@ -1,8 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 interface SectionProps
-    extends PropsWithChildren,
-        React.ComponentProps<'section'> {}
+    extends PropsWithChildren, React.ComponentProps<'section'> {}
 
 export default function Section({ children, ...props }: SectionProps) {
     return <section {...props}>{children}</section>;
