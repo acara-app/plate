2c0b8abc94711574d6ee63f7672141648697dba3
Tuvshu <tuvshinjargal.b@gmail.com>
2026-01-06 18:36:12 -0600

    feat: create migration for contents table

---
+++ database/migrations/2026_01_06_203438_create_contents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table): void {
+            $table->id();
+            $table->string('type')->index();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('meta_title');
+            $table->text('meta_description');
+            $table->json('body');
+            $table->string('image_path')->nullable();
+            $table->boolean('is_published')->default(true);
+            $table->timestamps();
+
+            $table->index(['type', 'is_published']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
