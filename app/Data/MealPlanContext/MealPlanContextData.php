eaed4952584d2ccbdbd79c19aff444d2bef91c19
Tuvshu <tuvshinjargal.b@gmail.com>
2026-04-23 16:26:03 -0600

    update privacy policy

--- app/Data/MealPlanContext/MealPlanContextData.php
+++ app/Data/MealPlanContext/MealPlanContextData.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace App\Data\MealPlanContext;
 
 use App\Data\GlucoseAnalysis\GlucoseAnalysisData;
-use App\Enums\BloodType;
 use App\Enums\DietType;
 use App\Enums\Sex;
-use Carbon\Carbon;
 use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapOutputName;
 use Spatie\LaravelData\Data;
@@ -26,11 +24,9 @@ final class MealPlanContextData extends Data
      */
     public function __construct(
         public ?int $age,
-        public ?Carbon $dateOfBirth,
         public ?float $height,
         public ?float $weight,
         public ?Sex $sex,
-        public ?BloodType $bloodType,
         public ?float $bmi,
         public ?float $bmr,
         public ?float $tdee,
